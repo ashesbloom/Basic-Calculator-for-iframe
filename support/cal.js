b992426c24fd15bd2fec1235db1060208cb69e48
ashes.bloom <mayankpandeydk123@gmail.com>
2024-01-11 20:47:46 +0530

    added calculations in js

--- support/cal.js
+++ support/cal.js
@@ -30,3 +30,95 @@ function keypre(event) {
     }
 }
 document.addEventListener('keydown', keypre);
+
+// Calculator logic
+var operand1 = '';
+var operator = '';
+var operand2 = '';
+
+function Buttonclick(value) {
+    if (value >= '0' && value <= '9') {
+        handleNumber(value);
+    } else if (value === '.') {
+        handleDecimal();
+    } else if (value === 'clear') {
+        clearCalculator();
+    } else if (value === '=') {
+        calculateResult();
+    } else {
+        handleOperator(value);
+    }
+
+    updateDisplay();
+}
+
+function handleNumber(value) {
+    if (operator === '') {
+        operand1 += value;
+    } else {
+        operand2 += value;
+    }
+}
+
+function handleDecimal() {
+    if (operator === '') {
+        if (!operand1.includes('.')) {
+            operand1 += '.';
+        }
+    } else {
+        if (!operand2 || (operand2 && !operand2.includes('.'))) {
+            operand2 += '.';
+        }
+    }
+}
+
+
+function handleOperator(value) {
+    if (operand1 !== '') {
+        operator = value;
+    }
+}
+
+var backspacePressTimer; 
+
+function startBackspacePress() {
+    backspacePressTimer = setTimeout(function () {
+        clearCalculator();
+        updateDisplay();
+    }, 700); 
+}
+
+function stopBackspacePress() {
+    clearTimeout(backspacePressTimer);
+}
+
+function backspace() {
+    if (operator === '' && operand1.length > 0) {
+        operand1 = operand1.slice(0, -1);
+    } else if (operand1.length > 0 && operator !== '' && operand2.length === 0) {
+        operator = '';
+    }else{
+        operand2 = operand2.slice(0, -1);
+    }
+
+    updateDisplay();
+}
+
+
+function clearCalculator() {
+    operand1 = '';
+    operator = '';
+    operand2 = '';
+}
+
+function calculateResult() {
+    if (operand1 !== '' && operator !== '' && operand2 !== '') {
+        var result = eval(operand1 + operator + operand2);
+        clearCalculator();
+        operand1 = result.toString();
+    }
+}
+
+function updateDisplay() {
+    document.getElementById('text_input').value = operand1 + operator + operand2;
+}
